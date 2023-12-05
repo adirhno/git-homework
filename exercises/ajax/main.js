71d7ebc62bb163902307f35185564e936eb65325
adirhno <adirhno@gmail.com>
2023-12-05 09:32:06 +0200

    update

--- exercises/ajax/main.js
+++ exercises/ajax/main.js
@@ -40,19 +40,39 @@
 // });
 
 //Exe3
-const fetch = function (isbn) {
-  $.ajax({
-    method: "GET",
-    url: `https://www.googleapis.com/books/v1/volumes?author&title`,
-    success: function (data) {
-      console.log(data);
-    },
-    error: function (xhr, text, error) {
-      console.log(text);
-    },
-  });
-};
+// const fetch = function (isbn) {
+//   $.ajax({
+//     method: "GET",
+//     url: `https://www.googleapis.com/books/v1/volumes?author&title`,
+//     success: function (data) {
+//       console.log(data);
+//     },
+//     error: function (xhr, text, error) {
+//       console.log(text);
+//     },
+//   });
+// };
+
+// $("#fetch").on("click", () => {
+//   fetch(1440633908);
+// });
+
+//Exe4
 
 $("#fetch").on("click", () => {
-  fetch(1440633908);
+  $.ajax({
+    method: "GET",
+
+    url: `http://api.giphy.com/v1/gifs/search?q=${$(
+      "#input"
+    ).val()}&api_key=v6NP6FvMrt7gnBBQUXpQs7Q0Uyejg8Rl&limit=5&embedded=true`,
+    success: function (result) {
+      console.log(result.data[0].url);
+      $("#iframe").attr(
+        "src",
+        "https://giphy.com/gifs/funny-cat-mlvseq9yvZhba"
+      );
+      console.log();
+    },
+  });
 });
