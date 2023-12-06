3265cf19bfabeee6c40e320f8a5d3df1db5346b3
adirhno <adirhno@gmail.com>
2023-12-06 09:36:54 +0200

    update

---
+++ exercises/OOP/main.js
@@ -0,0 +1,63 @@
+/** @format */
+
+//write your code here to make the tests pass
+
+class Document {
+  constructor(EmployeeName) {
+    this.EmployeeName = EmployeeName;
+  }
+}
+
+class Employee {
+  constructor(name) {
+    this.name = name;
+  }
+
+  work = function (office) {
+    let i = 0;
+    while (i < 10) {
+      office.documents.push(new Document(this.name));
+      i++;
+    }
+  };
+}
+class Manager {
+  constructor(name) {
+    this.name = name;
+    this.employees = [];
+  }
+  hireEmployee(name) {
+    this.employees.push(new Employee(name));
+  }
+
+  askEmployeesToWork() {
+    this.employees.forEach((e) => e.work(Office));
+  }
+}
+
+class Office {
+  constructor() {
+    this.documents = [];
+    this.managers = [];
+    this.cleaners = [];
+  }
+
+  hireCleaner(name) {
+    this.cleaners.push(new Cleaner(name));
+  }
+
+  hireManager(name) {
+    this.managers.push(new Manager(name));
+  }
+  startWorkDay() {
+    this.managers.forEach((e) => e.askEmployeesToWork());
+  }
+}
+
+class Cleaner {
+  constructor(name) {
+    this.name = name;
+  }
+
+  clean = () => console.log("Clean");
+}
